6eaddaa09ec5faaed56a4ab2855fb18a29c7f139
cluster1412 <clusterclan1@gmail.com>
2025-10-13 23:25:43 +0530

    13/10/25

--- Arrays/reversingArray.cpp
+++ Arrays/reversingArray.cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverseArray(vector<int> &arr , int m){
+    int n = arr.size();
+    int j = n-1;
+
+    for(int i = m+1;i<j;i++){
+        swap(arr[i],arr[j--]);
+    }
+}
+
 int main(){
-    cout<<"hi";
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    vector<int> arr(n);
+
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    cout<<"Entered array is :";
+    for(int i = 0; i < n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    int m;
+    cout<<"\n\nEnter the index from which you want to reverse: ";
+    cin>>m;
+    reverseArray(arr,m);
+    cout<<"\nReversed array is: ";
+    for(int i = 0; i < n ; i++){
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
